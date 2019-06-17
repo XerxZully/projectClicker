bc0dce2ad8ffe8c9f874b92535f0bd14ec78740c
XerxZully <Xerxxx@yandex.ru>
2019-06-17 21:19:57 +0300

    1

---
+++ Assets/Clicker.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+using Vector3 = System.Numerics.Vector3;
+
+public class Clicker : MonoBehaviour {
+    [SerializeField] private Material matDefault = null;
+    [SerializeField] private Material matHit = null;
+    private static int maxHealth = 1;
+    private int health = maxHealth;
+
+    private float resize;
+    private float sizeOnClickTarget;
+    private bool shrink = false;
+
+    public void init(int setHealth)
+    {
+        health = maxHealth = setHealth;
+    }
+
+    public bool changeHealth(int change)
+    {
+        health += change;
+        if (health < 1) {
+            kill();
+            return true; //object dead
+        }
+        
+        //RUN!!!
+        gameObject.GetComponent<Rigidbody>().AddTorque( Random.Range(-1000f,1000f), Random.Range(-1000f,1000f), Random.Range(-1000f,1000f) );
+        gameObject.GetComponent<Rigidbody>().AddForce( Random.Range(-800f,800f), Random.Range(-800f,800f), 0 );
+        
+        sizeOnClickTarget = Mathf.Max( 0.75f, gameObject.transform.localScale.x * 0.75f );
+        shrink = true;
+        gameObject.GetComponent<MeshRenderer>().material = matHit;
+        return false;
+    }
+
+    public void kill()
+    {
+        Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        gameObject.transform.localScale *= Random.Range(0.8f, 1.5f);
+        gameObject.GetComponent<Rigidbody>().AddTorque( Random.Range(-100f,100f), Random.Range(-100f,100f), Random.Range(-100f,100f) );
+        gameObject.GetComponent<Rigidbody>().AddForce( Random.Range(-300f,300f), Random.Range(-300f,300f), 0 );
+    }
+
+    private void Update()
+    {
+        if (shrink)
+            if (gameObject.transform.localScale.x > sizeOnClickTarget) {
+                resize = Mathf.Lerp(1f, 0.5f, Time.deltaTime * 3f);
+                gameObject.transform.localScale *= resize;
+            } else {
+                shrink = false;
+                gameObject.GetComponent<MeshRenderer>().material = matDefault;
+            }
+
+        if ( gameObject.transform.localScale.x < 3f )
+            gameObject.transform.localScale *= 1.002f;
+    }
+}
---
+++ Assets/GameManager.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.UI;
+using Vector3 = System.Numerics.Vector3;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager manager = null;
+
+    [SerializeField] private Transform obj = null;
+    [SerializeField] private Text scoreText = null;
+    [SerializeField] private Text gameOverText = null;
+    [SerializeField] private Button repeatButton = null;
+    [SerializeField] private int maxObjects = 5;
+    [SerializeField] private float summonInterval = 1.0f;
+    [SerializeField] private int maxHealth = 5;
+
+    private bool ingame = true;
+    private int objects = 0;
+    private int score = 0;
+    private float[] corners = { -7.0f, -4.0f, 7.0f, 4.0f }; //x_min, y_min, x_max, y_max
+
+    public static GameManager Instanse { get; private set; } = null;
+    
+    private void Awake()
+    {
+        Instanse = this;
+        
+        init();
+    }
+
+    private void init()
+    {
+        obj.gameObject.GetComponent<Clicker>().init(maxHealth);
+        repeatButton.onClick.AddListener( RepeatButtonOnClick );
+        newGame();
+    }
+
+    private void newGame()
+    {
+        score = 0;
+        scoreText.text = "Score: 0";
+        objects = 0;
+        ingame = true;
+        gameOverText.enabled = !ingame;
+        repeatButton.gameObject.SetActive(!ingame);
+        
+        GameObject[] list = GameObject.FindGameObjectsWithTag("Clickable");
+        foreach ( GameObject item in list)
+            item.gameObject.GetComponent<Clicker>().kill();
+
+        InvokeRepeating("SummonCube", 1.0f, summonInterval );
+    }
+
+    private void endGame()
+    {
+        ingame = false;
+        gameOverText.enabled = !ingame;
+        repeatButton.gameObject.SetActive(!ingame);
+        CancelInvoke("SummonCube");
+    }
+    
+    public void RepeatButtonOnClick()
+    {
+        newGame();
+    }
+    
+    private void Update()
+    {
+        if ( ingame && Input.GetMouseButtonDown(0)) {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if ( Physics.Raycast(ray, out hit) ) {
+                if ( hit.transform.tag == "Clickable") {
+                    if (hit.collider.gameObject.GetComponent<Clicker>().changeHealth(-1))
+                        objects--;
+                    addScore(1);
+                }
+            }
+        }
+    }
+
+    public void addScore(int value)
+    {
+        score += value;
+        scoreText.text = "Score: " + score.ToString();
+    }
+
+    private void SummonCube()
+    {
+        Transform inst;
+    
+        inst = Instantiate( obj );
+        inst.position = new UnityEngine.Vector3( Random.Range(corners[0],corners[2]), Random.Range(corners[1],corners[3]), 0 );
+        objects++;
+        if (objects >= maxObjects)
+            endGame();
+    }
+}
